6c7d50219bbbf9aa039300eb606d307f26c703e2
Pedro-Sakata <22186@gdc.school.nz>
2025-08-26 13:40:54 +1200

    imports and root

---
+++ EoYP.py
@@ -0,0 +1,16 @@
+#imports the tkinter library
+from tkinter import *
+from tkinter import ttk 
+from PIL import Image, ImageTk
+
+#constants 
+mons_max = 10
+score_maximum = 25
+score_minimum = 1
+name_max = 16
+
+root = Tk()
+root.title ("Monster Catalogue")
+root.geometry("840x470")
+
+root.mainloop()
